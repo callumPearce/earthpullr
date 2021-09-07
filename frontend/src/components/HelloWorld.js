46c4ea21f3eb83a370a68102b71ae94475192b1e
Callum <callumpearce@improbable.io>
2021-09-07 21:31:49 +0100

    Connected front-end to backend, now on button press images are retrieved

--- frontend/src/components/HelloWorld.js
+++ frontend/src/components/HelloWorld.js
@@ -5,29 +5,17 @@ function HelloWorld() {
 	const [showModal, setShowModal] = useState(false);
 	const [result, setResult] = useState(null);
 
-	const handleOpenModal = () => {
-		setShowModal(true);
-
-		window.backend.basic().then((result) => setResult(result));
-	};
-
-	const handleCloseModal = () => {
-		setShowModal(false);
-	};
-
+	function handleGetNewImages() {
+		window.backend.RetrieveImages().then(result =>
+			console.log(result)
+		);
+	}
+	
 	return (
 		<div className="App">
-			<button onClick={() => handleOpenModal()} type="button">
-				Hello
-      </button>
-			<Modal
-				appElement={document.getElementById("app")}
-				isOpen={showModal}
-				contentLabel="Minimal Modal Example"
-			>
-				<p>{result}</p>
-				<button onClick={() => handleCloseModal()}>Close Modal</button>
-			</Modal>
+			<button onClick={() => handleGetNewImages()} type="button">
+				Get New Images
+      		</button>
 		</div>
 	);
 }
