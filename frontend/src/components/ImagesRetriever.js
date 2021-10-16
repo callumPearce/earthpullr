259a3da5445e1dd32facf5dce34905d3a68ef822
Callum <callumpearce@improbable.io>
2021-10-16 22:22:14 +0100

    Added initial layout and progress bar

--- frontend/src/components/ImagesRetriever.js
+++ frontend/src/components/ImagesRetriever.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import * as Wails from '@wailsapp/runtime'
-import { FormControl, Input, TextField, Button } from '@mui/material';
+import PropTypes from 'prop-types';
+import { FormControl, Container, Box,	 Grid, TextField, InputAdornment, Button, Typography, LinearProgress } from '@mui/material';
 
 const MAX_RES = 7680
 const MAX_BACKGROUND_COUNT = 50
@@ -35,16 +36,40 @@ const downloadPathValidation = path => {
 	return null;
 }
 
+ProgressBar.propTypes = {
+	/**
+	 * The value of the progress indicator for the determinate and buffer variants.
+	 * Value between 0 and 100.
+	 */
+	value: PropTypes.number.isRequired,
+};
+
+
+function ProgressBar(props) {
+	return (
+		<Box sx={{ display: 'flex', alignItems: 'center' }}>
+			<Box sx={{ width: '100%', mr: 1 }}>
+				<LinearProgress variant="determinate" {...props} />
+			</Box>
+			<Box sx={{ minWidth: 35 }}>
+				<Typography variant="body2" color="text.secondary">{`${Math.round(
+					props.value,
+				)}%`}</Typography>
+			</Box>
+		</Box>
+	);
+}
+
 class ImagesRetriever extends React.Component {
 	constructor(props, context) {
 		super(props);
 
 		this.state = {
 			form: {
-				backgroundsCount: {value: 1, errMsg: "", valid: true, validator: backgroundCountValidation},
+				backgroundsCount: {value: null, errMsg: "", valid: true, validator: backgroundCountValidation},
 				imagesWidth: {value: window.screen.width * window.devicePixelRatio, errMsg: "", valid: true, validator: imageDimensionValidation},
 				imagesHeight: {value: window.screen.height * window.devicePixelRatio, errMsg: "", valid: true, validator: imageDimensionValidation},
-				downloadPath: {value: "", errMsg: "", valid: false, validator: downloadPathValidation},
+				downloadPath: {value: "", errMsg: "", valid: true, validator: downloadPathValidation},
 			},
 			imagesSaved: 0,
 			responseMsg: ""
@@ -113,63 +138,82 @@ class ImagesRetriever extends React.Component {
 
 	render() {
 		return (
-			<div className="App">
+			<Container component="div" className="App" sx={{ width: '34ch' }}>
 				<FormControl action="#" noValidate>
-					<TextField
-						type="number"
-						label="Number of Backgrounds"
-						name="backgroundsCount"
-						value={this.state.form.backgroundsCount.value}
-						onChange={this.handleUserInput}
-						error={!this.state.form.backgroundsCount.valid}
-						helperText={this.state.form.backgroundsCount.errMsg}
-						required
-					/>
-					<TextField
-						type="number"
-						label="Image Width (px)"
-						name="imagesWidth"
-						value={this.state.form.imagesWidth.value}
-						onChange={this.handleUserInput}
-						error={!this.state.form.imagesWidth.valid}
-						helperText={this.state.form.imagesWidth.errMsg}
-						required
-					/>
-					<TextField
-						type="number"
-						label="Image Height (px)"
-						name="imagesHeight"
-						value={this.state.form.imagesHeight.value}
-						onChange={this.handleUserInput}
-						error={!this.state.form.imagesHeight.valid}
-						helperText={this.state.form.imagesHeight.errMsg}
-						required
-					/>
-					<TextField
-						type="text"
-						label= "Download Path"
-						name="downloadPath"
-						value={this.state.form.downloadPath.value}
-						onChange={this.handleUserInput}
-						error={!this.state.form.downloadPath.valid}
-						helperText={this.state.form.downloadPath.errMsg}
-						required
-					/>
-					<Button
-						type="submit"
-						variant="contained"
-						onClick={this.handleSubmit}
-					>
-						Get New Images
-					</Button>
+					<Grid container spacing={2}>
+						<Grid item xs={12}>
+							<TextField
+								type="text"
+								label= "Download Path"
+								name="downloadPath"
+								value={this.state.form.downloadPath.value}
+								onChange={this.handleUserInput}
+								error={!this.state.form.downloadPath.valid}
+								helperText={this.state.form.downloadPath.errMsg}
+								sx={{width: "14.5ch", m: .5}}
+								required
+							/>
+						</Grid>
+						<Grid item xs={12}>
+							<TextField
+								type="number"
+								label="Width"
+								name="imagesWidth"
+								value={this.state.form.imagesWidth.value}
+								onChange={this.handleUserInput}
+								error={!this.state.form.imagesWidth.valid}
+								helperText={this.state.form.imagesWidth.errMsg}
+								InputProps={{endAdornment: <InputAdornment position="end">px</InputAdornment>}}
+								sx={{width: "7ch", m: .5}}
+								required
+							/>
+							<TextField
+								type="number"
+								label="Height"
+								name="imagesHeight"
+								value={this.state.form.imagesHeight.value}
+								onChange={this.handleUserInput}
+								error={!this.state.form.imagesHeight.valid}
+								helperText={this.state.form.imagesHeight.errMsg}
+								InputProps={{endAdornment: <InputAdornment position="end">px</InputAdornment>}}
+								sx={{width: "7ch", m: .5}}
+								required
+							/>
+						</Grid>
+						<Grid item xs={12}>
+							<TextField
+								type="number"
+								label="Backgrounds"
+								name="backgroundsCount"
+								value={this.state.form.backgroundsCount.value}
+								onChange={this.handleUserInput}
+								error={!this.state.form.backgroundsCount.valid}
+								helperText={this.state.form.backgroundsCount.errMsg}
+								inputProps={{min: 0, style: { textAlign: 'center' }}}
+								sx={{width: "14.5ch", m: .5}}
+								required
+							/>
+						</Grid>
+						<Grid item xs={12}>
+							<Button
+								type="submit"
+								variant="contained"
+								onClick={this.handleSubmit}
+							>
+								Get New Images
+							</Button>
+						</Grid>
+						<Grid item xs={12}>
+							<ProgressBar value={(this.state.imagesSaved/this.state.form.backgroundsCount.value)*100}/>
+						</Grid>
+						<Grid item xs={12}>
+							<p>
+								{this.state.responseMsg.toString()}
+							</p>
+						</Grid>
+					</Grid>
 				</FormControl>
-				<p>
-					{this.state.imagesSaved.toString()}
-				</p>
-				<p>
-					{this.state.responseMsg.toString()}
-				</p>
-			</div>
+			</Container>
 		);
 	}
 }
